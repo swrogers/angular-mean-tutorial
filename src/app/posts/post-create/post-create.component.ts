7cf27153310f52b1ac90120a0416c6003edffcd4
Shane Rogers <swrogers@gmail.com>
2020-01-25 13:30:07 -0600

    Getting User Input
    Using element references, one way data binding;
    Using ngModel and two way data binding.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
 })
 export class PostCreateComponent {
   newPost = 'NO CONTENT';
+  enteredValue = '';
 
   onAddPost() {
-    this.newPost = 'The users new post';
+    this.newPost = this.enteredValue;
   }
 }
