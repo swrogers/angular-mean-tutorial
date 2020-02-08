bca8a40c562104b8e93c21572d00f8e93c186ce8
Shane Rogers <swrogers@gmail.com>
2020-02-08 14:35:16 -0600

    NodeJS - Fetching initial posts
    Resolving an initial batch of fake posts to the client in json format.

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fakejkljekl',
+      title: 'First server side post',
+      content: 'This comes from the server'
+    },
+    {
+      id: 'rjeifmnieaonfeio',
+      title: 'Second server side post',
+      content: 'More content from the server.'
+    }
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
+
 });
 
 module.exports = app;
