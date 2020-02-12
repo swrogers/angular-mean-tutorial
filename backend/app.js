c5ed5c0b2f0e2df5084be1f4e6ba7eb806d8dff4
Shane Rogers <swrogers@gmail.com>
2020-02-12 14:24:38 -0600

    MongoDB - GETting an item from the DB
    Fetching posts from the Database.

--- backend/app.js
+++ backend/app.js
@@ -42,24 +42,13 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fakejkljekl',
-      title: 'First server side post',
-      content: 'This comes from the server'
-    },
-    {
-      id: 'rjeifmnieaonfeio',
-      title: 'Second server side post',
-      content: 'More content from the server.'
-    }
-  ];
-
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
-
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
