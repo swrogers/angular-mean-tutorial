6f6e424b23c1011b61fceee8f0dc0187957a628e
Shane Rogers <swrogers@gmail.com>
2020-02-15 10:24:09 -0600

    Enhancements - Reorganize the backend routing
    Moved all current routes into new subfolder using express router.

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post.js');
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 
@@ -27,60 +27,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  post.save()
-    .then(createdPost => {
-      res.status(201).json({
-        message: 'Post added successfully!',
-        postId: createdPost._id
-      });
-    });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: documents
-      });
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if(post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: 'Post not found'});
-    }
-  });
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  Post.updateOne({_id: req.params.id}, post ).then(result => {
-    res.sent(200).json({message: "Update Successful!"});
-  });
-});
-
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  console.log(req.params.id);
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({message: "post deleted"});
-  });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
