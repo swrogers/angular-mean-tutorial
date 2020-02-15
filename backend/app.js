384a8118b926ede58ec626e3a26746f8677eaaef
Shane Rogers <swrogers@gmail.com>
2020-02-15 09:57:31 -0600

    Enhancements - Updating posts on the server
    Also fixed an introduced bug using the '?' operator on the template, for when
    initially loading the edit form and the post hasn't been actually properly loaded.

--- backend/app.js
+++ backend/app.js
@@ -52,6 +52,15 @@ app.get('/api/posts', (req, res, next) => {
     });
 });
 
+app.get('/api/posts/:id', (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if(post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({message: 'Post not found'});
+    }
+  });
+});
 
 app.put('/api/posts/:id', (req, res, next) => {
   const post = new Post({
@@ -61,7 +70,6 @@ app.put('/api/posts/:id', (req, res, next) => {
   });
 
   Post.updateOne({_id: req.params.id}, post ).then(result => {
-    console.log(result);
     res.sent(200).json({message: "Update Successful!"});
   });
 });
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -33,7 +33,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return {...this.posts.find(p => p.id === id)};
+    return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
   }
 
   getPostUpdateListener() {
@@ -56,7 +56,11 @@ export class PostsService {
     const post: Post = { id: id, title: title, content: content };
     this.http.put("http://localhost:3000/api/posts/" + id, post)
       .subscribe(response => {
-        console.log(response);
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdate.next([...this.posts]);
       });
   }
 
