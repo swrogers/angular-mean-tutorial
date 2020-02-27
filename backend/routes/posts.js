fa0d037f71d46f5c7c42122f2a42c5015662a4c9
Shane Rogers <swrogers@gmail.com>
2020-02-27 14:56:51 -0600

    Backend - Adding server side upload
    Uses multer package to enable server side upload for png and jpg files.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const multer = require('multer');
 
 const router = express.Router();
 
 const Post = require('../models/post.js');
 
-router.post('', (req, res, next) => {
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mimetype');
+    if(isValid) {
+      error = null;
+    }
+
+    callback(error, 'backend/images');
+  },
+
+  filename: (req, file, callback) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    callback(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+
+router.post('', multer(storage).single('image') ,(req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
