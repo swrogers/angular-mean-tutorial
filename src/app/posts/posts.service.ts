11b0163802fd5589bdbc6d9e2a8d6b1dca9f00cc
Shane Rogers <swrogers@gmail.com>
2020-03-03 11:32:51 -0600

    File Upload URL from backend to frontend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -24,7 +24,8 @@ export class PostsService {
           return {
             title: post.title,
             content: post.content,
-            id: post._id
+            id: post._id,
+            imagePath: post.imagePath
           };
         });
       }))
@@ -48,9 +49,14 @@ export class PostsService {
     postData.append('content', content);
     postData.append('image', image, title);
 
-    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', postData)
+    this.http.post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData)
       .subscribe((responseData) => {
-        const post: Post = { id: responseData.postId, title: title, content: content};
+        const post: Post = {
+          id: responseData.post.id,
+          title: title,
+          content: content,
+          imagePath: responseData.post.imagePath
+        };
 
         this.posts.push(post);
         this.postsUpdate.next([...this.posts]);
@@ -60,7 +66,7 @@ export class PostsService {
 
 
   updatePost(id: string, title: string, content: string) {
-    const post: Post = { id: id, title: title, content: content };
+    const post: Post = { id: id, title: title, content: content, imagePath: null };
     this.http.put("http://localhost:3000/api/posts/" + id, post)
       .subscribe(response => {
         const updatedPosts = [...this.posts];
